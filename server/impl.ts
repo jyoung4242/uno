03028d5da8b3f6f073641a3c94b4b51d14d7c775
Justin Young <justin_dean_young@yahoo.com>
2022-04-27 09:39:32 -0400

    updated

--- server/impl.ts
+++ server/impl.ts
@@ -1,42 +1,32 @@
-import { Methods, Context } from "./.hathora/methods";
-import { Response } from "../api/base";
-import {
-  Color,
-  Card,
-  PlayerState,
-  UserId,
-  IInitializeRequest,
-  IJoinGameRequest,
-  IStartGameRequest,
-  IPlayCardRequest,
-  IDrawCardRequest,
-} from "../api/types";
+import { Methods, Context } from './.hathora/methods';
+import { Response } from '../api/base';
+import { Color, Card, PlayerState, UserId, IInitializeRequest, IJoinGameRequest, IStartGameRequest, IPlayCardRequest, IDrawCardRequest } from '../api/types';
 
 type InternalState = PlayerState;
 
 export class Impl implements Methods<InternalState> {
-  initialize(ctx: Context, request: IInitializeRequest): InternalState {
-    return {
-      hand: [],
-      players: [],
-      turn: userId,
-      pile: undefined,
-      winner: undefined,
-    };
-  }
-  joinGame(state: InternalState, userId: UserId, ctx: Context, request: IJoinGameRequest): Response {
-    return Response.error("Not implemented");
-  }
-  startGame(state: InternalState, userId: UserId, ctx: Context, request: IStartGameRequest): Response {
-    return Response.error("Not implemented");
-  }
-  playCard(state: InternalState, userId: UserId, ctx: Context, request: IPlayCardRequest): Response {
-    return Response.error("Not implemented");
-  }
-  drawCard(state: InternalState, userId: UserId, ctx: Context, request: IDrawCardRequest): Response {
-    return Response.error("Not implemented");
-  }
-  getUserState(state: InternalState, userId: UserId): PlayerState {
-    return state;
-  }
+    initialize(ctx: Context, userId: UserId, request: IInitializeRequest): InternalState {
+        return {
+            hand: [],
+            players: [],
+            turn: userId,
+            pile: undefined,
+            winner: undefined,
+        };
+    }
+    joinGame(state: InternalState, userId: UserId, ctx: Context, request: IJoinGameRequest): Response {
+        return Response.error('Not implemented');
+    }
+    startGame(state: InternalState, userId: UserId, ctx: Context, request: IStartGameRequest): Response {
+        return Response.error('Not implemented');
+    }
+    playCard(state: InternalState, userId: UserId, ctx: Context, request: IPlayCardRequest): Response {
+        return Response.error('Not implemented');
+    }
+    drawCard(state: InternalState, userId: UserId, ctx: Context, request: IDrawCardRequest): Response {
+        return Response.error('Not implemented');
+    }
+    getUserState(state: InternalState, userId: UserId): PlayerState {
+        return state;
+    }
 }
